429f1a38e38104447070794b1bdd0bf495ada2a9
TokenMaster Global <qianzhouxia@gmail.com>
2026-07-13 23:59:46 +0800

    fix: add lib/gtag.ts

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -1,13 +1,13 @@
-export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID || ;
+export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID || "";
 
 export const pageview = (url: string) => {
-  if (typeof window !== undefined && (window as any).gtag) {
-    (window as any).gtag(config, GA_TRACKING_ID, { page_path: url });
+  if (typeof window !== "undefined" && (window as any).gtag) {
+    (window as any).gtag("config", GA_TRACKING_ID, { page_path: url });
   }
 };
 
 export const event = (action: string, params: Record<string, any>) => {
-  if (typeof window !== undefined && (window as any).gtag) {
-    (window as any).gtag(event, action, params);
+  if (typeof window !== "undefined" && (window as any).gtag) {
+    (window as any).gtag("event", action, params);
   }
 };
